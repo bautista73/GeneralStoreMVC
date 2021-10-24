944c88a99fec91ce0ef1eca3a99bf4e9064b1772
Brandon Bautista <brandonbautista731@hotmail.com>
2021-10-23 23:20:47 -0500

    Updated PC

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,12 +10,15 @@ namespace GeneralStoreMVC.Models
     {
         [Key]
         public int CustomerId { get; set; }
+
         [Required]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
         [Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         [Display(Name = "Full Name")]
         public string FullName { get; set; }
     }
